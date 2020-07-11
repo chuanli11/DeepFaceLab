fa6798023838d8c96dcdd8279538ec24effdffc7
chuanli11 <cl.chuanli@gmail.com>
2020-07-11 15:12:06 -0700

    Benchmarked on HP16

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 if __name__ == "__main__":
     # Fix for linux
     import multiprocessing
-    multiprocessing.set_start_method("fork")
+    multiprocessing.set_start_method("spawn")
 
     from core.leras import nn
     nn.initialize_main_env()
