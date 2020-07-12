09496e611486f44099cc21c4de6b2348bf2480ba
chuanli11 <cl.chuanli@gmail.com>
2020-07-11 22:58:56 -0700

    Add more configurations for benchmark

--- gather_benchmark.py
+++ gather_benchmark.py
@@ -5,11 +5,16 @@ path_config = 'benchmark/log'
 output_file = 'benchmark/benchmark.csv'
 
 list_gpu_type = ['TeslaV100-SXM3-32GB']
+
 list_config = [
-# 'LambdaSAEHD_liae_128_128_64_64', 'LambdaSAEHD_liae_128_128_64_64_syn', 'LambdaSAEHD_liae_256_128_64_64', 'LambdaSAEHD_liae_256_128_64_64_syn', \
-# 'LambdaSAEHD_liae_gan_128_128_64_64', 'LambdaSAEHD_liae_gan_128_128_64_64_syn', 'LambdaSAEHD_liae_gan_256_128_64_64', 'LambdaSAEHD_liae_gan_256_128_64_64_syn', \
-'LambdaSAEHD_liae_128_256_128_128', 'LambdaSAEHD_liae_128_256_128_128_syn'
+'LambdaSAEHD_liae_128_128_64_64', 'LambdaSAEHD_liae_128_128_64_64_syn', 'LambdaSAEHD_liae_256_128_64_64', 'LambdaSAEHD_liae_256_128_64_64_syn', \
+'LambdaSAEHD_liae_gan_128_128_64_64', 'LambdaSAEHD_liae_gan_128_128_64_64_syn', 'LambdaSAEHD_liae_gan_256_128_64_64', 'LambdaSAEHD_liae_gan_256_128_64_64_syn', \
+'LambdaSAEHD_liae_128_256_128_128', 'LambdaSAEHD_liae_128_256_128_128_syn' \
+'LambdaSAEHD_liae_fp_128_128_64_64', 'LambdaSAEHD_liae_fp_128_128_64_64_syn' \
+'LambdaSAEHD_liae_fs_128_128_64_64', 'LambdaSAEHD_liae_fs_128_128_64_64_syn' \
+'LambdaSAEHD_liae_bs_128_128_64_64', 'LambdaSAEHD_liae_bs_128_128_64_64_syn'
 ]
+
 list_gpu_idxs = ['0', '0,1', '0,1,2,3', '0,1,2,3,4,5,6,7', '0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15']
 pattern = "]["
 
