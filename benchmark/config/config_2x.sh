2dddda3e07bbb31077fda68bc1f86f566c44b6f8
chuanli11 <cl.chuanli@gmail.com>
2020-07-15 23:15:25 -0700

    Add configurations to debug fp16 nan loss

--- benchmark/config/config_2x.sh
+++ benchmark/config/config_2x.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 CONFIGS="\
-LambdaSAEHD_liae_128_128_64_64 \
+LambdaSAEHD_liae_ud_gan_416_128_64_64 \
 "
 
-GPU_IDXS="0,1"
+GPU_IDXS="0"
 
 # LambdaSAEHD_liae_128_128_64_64 LambdaSAEHD_liae_256_128_64_64 LambdaSAEHD_liae_512_128_64_64
